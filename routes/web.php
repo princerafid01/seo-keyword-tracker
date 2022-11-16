ff9b44c1ff8b53e17bf5455f41d267bb649076f3
Mahmud Rafid <princerafid01@gmail.com>
2022-11-17 01:55:37 +0600

    Initialized

---
+++ routes/web.php
@@ -0,0 +1,21 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use Yajra\DataTables\DataTables;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    // Using Eloquent
+    // return DataTables::eloquent(collect(['' => '']))->make(true);
+    return view('welcome');
+});
